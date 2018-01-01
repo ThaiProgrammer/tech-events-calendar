edd8dfb5f7a8f36f63a53579ff8a44942b56efed
Maythee Anegboonlap <null@llun.me>
2018-01-01 23:36:45 +0800

    Generate ics file per event

--- ics.js
+++ ics.js
@@ -1,3 +1,4 @@
+const ics = require('ics')
 const peg = require('pegjs')
 const fs = require('fs')
 
@@ -6,5 +7,78 @@ const parser = peg.generate(grammar)
 
 const content = fs.readFileSync('./README.md', { encoding: 'utf8' })
 const eventsData = content.substring(0, content.indexOf('## Symbol Legend'))
-const events = parser.parse(eventsData)
-console.log(events)
+const document = parser.parse(eventsData)
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+async function createIcs() {
+  return Promise.all(
+    document.events
+      .map(event => {
+        const header = event.header
+        const content = event.content
+
+        const year = parseInt(document.year, 10)
+        const month = months.indexOf(document.month) + 1
+
+        let start = [year, month, header.day.from, 0, 0]
+        let end = []
+        if (header.day.to) {
+          end = [year, month, header.day.to, 0, 0]
+        }
+        if (content.time) {
+          const firstTime = content.time[0]
+          const lastTime = content.time[content.time.length - 1]
+          start = [
+            year,
+            month,
+            header.day.from,
+            firstTime.from.hour,
+            firstTime.from.minute
+          ]
+          end = [
+            year,
+            month,
+            header.day.from,
+            lastTime.to.hour,
+            lastTime.to.minute
+          ]
+        }
+        return {
+          start,
+          end,
+          categories: [content.topic.type],
+          title: header.title,
+          location: content.location && content.location.location,
+          description: content.th_summary + content.en_summary
+        }
+      })
+      .map(item => {
+        return new Promise((resolve, reject) => {
+          ics.createEvent(item, (error, value) => {
+            if (error) return reject(error)
+            resolve([value, item])
+          })
+        })
+      })
+  )
+}
+
+createIcs().then(events => {
+  events.forEach(([ics, item]) => {
+    fs.writeFileSync(`${item.title}.ics`, ics)
+  })
+})
