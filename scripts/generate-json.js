a8d34b40c4b95e7e47119022f785542c4d3fa52d
Thai Pangsakulyanont <dtinth@spacet.me>
2018-01-02 22:25:45 +0700

    :sparkles: Save error to `tmp/readme-parse-errors.json`

--- scripts/generate-json.js
+++ scripts/generate-json.js
@@ -3,11 +3,25 @@ const fs = require('fs')
 const parseReadme = require('../lib/parseReadme')
 
 function main () {
-  const rawContent = fs.readFileSync('./README.md', { encoding: 'utf8' })
-  const json = parseReadme(rawContent, 'README.md')
-  const path = 'public/calendar.json'
-  fs.writeFileSync(path, JSON.stringify(json, null, 2))
-  console.log('* Written calendar to', path)
+  try {
+    const rawContent = fs.readFileSync('./README.md', { encoding: 'utf8' })
+    const json = parseReadme(rawContent, 'README.md')
+    const path = 'public/calendar.json'
+    fs.writeFileSync(path, JSON.stringify(json, null, 2))
+    console.log('* Written calendar to', path)
+  } catch (e) {
+    if (!e.location) {
+      throw e
+    }
+    require('child_process').execSync('mkdir -p tmp')
+    const path = 'tmp/readme-parse-errors.json'
+    fs.writeFileSync(path, JSON.stringify([{
+      message: e.message,
+      location: e.location
+    }], null, 2))
+    console.log('* Parse error written to', path)
+    throw e
+  }
 }
 
 main()
