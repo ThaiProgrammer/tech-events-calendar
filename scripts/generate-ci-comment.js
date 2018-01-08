382228e31fa9f69ca1092061c62fbc651c6410f5
Thai Pangsakulyanont <dtinth@spacet.me>
2018-01-08 14:05:07 +0700

    :sparkles: Instead of json-diff (fails at diffing array), use text diff

--- scripts/generate-ci-comment.js
+++ scripts/generate-ci-comment.js
@@ -39,12 +39,8 @@ if (fs.existsSync('public/calendar.json')) {
   try {
     console.error('# Now diffing calendar.json file.')
     execSync('curl https://thaiprogrammer-tech-events-calendar.spacet.me/calendar.json > /tmp/master-calendar.json', { timeout: 10000 })
-    const diffResult = require('json-diff').diffString(
-      require('/tmp/master-calendar'),
-      require('../public/calendar'),
-      { color: false }
-    )
-    if (diffResult) {
+    const diffResult = execSync('diff -u /tmp/master-calendar public/calendar || true', { timeout: 2000 })
+    if (diffResult.toString().trim()) {
       console.log(':bulb: **ข้อมูลปฏิทินมีการเปลี่ยนแปลง (Calendar data has been changed):**')
       console.log('```diff')
       console.log(diffResult)
