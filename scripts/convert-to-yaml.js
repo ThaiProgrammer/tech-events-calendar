85a6bc2bdbe9527c6ab04d9d80e8d4370b9ce632
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-02-15 22:54:27 +0700

    :sparkles: :tada: Initial work on YAML generator

---
+++ scripts/convert-to-yaml.js
@@ -0,0 +1,62 @@
+// Convert to JSON
+
+const data = require('../public/calendar')
+const yaml = require('js-yaml')
+
+for (const event of data) {
+  dumpEvent(event)
+}
+
+function dumpEvent (event) {
+  const filename = `data/${event.start.year}-${String(event.start.month).padStart(2, '0')}/${event.id}`
+  const attributes = {
+    id: event.id,
+    title: event.title,
+    date: dumpDate(event),
+  }
+  if (event.time) {
+    attributes.time = event.time.length === 1 ? dumpTime(event.time[0]) : event.time.map(dumpTime)
+  }
+  if (event.location) {
+    attributes.location = event.location
+  }
+  Object.assign(attributes, {
+    categories: event.categories,
+    topics: event.topics,
+    links: event.links.map(dumpLink)
+  })
+  if (event.summary) {
+    Object.assign(attributes, {
+      summary: event.summary
+    })
+  }
+  console.log('---\n' + yaml.safeDump(attributes) + '---\n' + event.description)
+}
+
+function dumpDate (event) {
+  const format = (d) => {
+    return `${d.year}-${String(d.month).padStart(2, '0')}-${String(d.date).padStart(2, '0')}`
+  }
+  const start = format(event.start)
+  const end = format(event.end)
+  return start + (start === end ? '' : ' ~ ' + end)
+}
+
+function dumpTime (time) {
+  const format = (t) => {
+    return String(t.hour).padStart(2, '0') + ':' + String(t.minute).padStart(2, '0')
+  }
+  const start = format(time.from)
+  const end = format(time.to)
+  return start + ' ~ ' + end + (time.after ? '++' : '') + (time.agenda ? ` (${time.agenda})` : '')
+}
+
+function dumpLink (link) {
+  const out = {
+    type: link.type
+  }
+  if (link.url) out.url = link.url
+  if (link.title) out.title = link.title
+  if (link.detail) out.detail = link.detail
+  return out
+}
