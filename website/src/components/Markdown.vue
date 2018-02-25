03349a36db42a96265028d2a9646ab937a676bbb
Suksit Sripitchayaphan <suksit@gmail.com>
2018-02-25 23:32:19 +0700

    Add Markdown component

---
+++ website/src/components/Markdown.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-html="renderedText"></div>
+</template>
+
+<script>
+import MarkdownIt from 'markdown-it'
+
+const md = MarkdownIt()
+
+export default {
+  props: ['text'],
+  computed: {
+    renderedText () {
+      return md.render(this.text)
+    }
+  }
+}
+</script>
