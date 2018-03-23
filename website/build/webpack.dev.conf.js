9ba80c5c90a0535e400398c14bb7ca9857cc5615
suksit <suksit@gmail.com>
2018-03-24 00:54:51 +0700

    Website should load data from local server

--- website/build/webpack.dev.conf.js
+++ website/build/webpack.dev.conf.js
@@ -29,7 +29,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       ]
     },
     hot: true,
-    contentBase: false, // since we use CopyWebpackPlugin.
+    contentBase: path.resolve(__dirname, '../../public'),
     compress: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
