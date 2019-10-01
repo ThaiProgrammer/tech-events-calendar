fb66c30fb8cb8a521634364fc295c67a1bd114a4
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2019-10-01 21:42:35 +0700

    Add link from homepage to event list

--- website/src/components/HomePage.vue
+++ website/src/components/HomePage.vue
@@ -4,7 +4,10 @@
       <h2 class="Subhead-heading">Check out the new calendar!</h2>
     </div>
     <p>
-      This calendar is deprecated. But worry not! You can find the shiny new calendar at https://th.techcal.dev/
+      This calendar is deprecated. But worry not! You can find the shiny new calendar at <a href="https://th.techcal.dev/">https://th.techcal.dev</a>.
+    </p>
+    <p>
+      Or see our past events in our <router-link to="/list">event list</router-link>.
     </p>
   </div>
 </template>
